ad84259787f8a5262adf6774fad5baddc2db1657
mskasal <mustafasamedkasal@gmail.com>
2021-07-19 15:42:42 +0200

    Health check directories

---
+++ lib/src/features/health_check/health_check.component.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HealthCheck extends StatelessWidget {
+  const HealthCheck({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
